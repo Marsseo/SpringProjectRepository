0a0fd7e21876c8ccb7def19466c6c326ec097fb1
cheolmin-Kim <kcm7582@naver.com>
2017-06-28 17:50:08 +0900

    sss

--- SpringWebProject/src/main/java/com/mycompany/myapp/controller/IpAdress.java
+++ SpringWebProject/src/main/java/com/mycompany/myapp/controller/IpAdress.java
@@ -1,11 +1,8 @@
 package com.mycompany.myapp.controller;
 
 public class IpAdress {
-<<<<<<< HEAD
 	private static String ipAddress="192.168.3.48";
-=======
-	private static String ipAddress="192.168.3.54";
->>>>>>> origin/master
+
 
 	public static String getIpAddress() {
 		return ipAddress;
