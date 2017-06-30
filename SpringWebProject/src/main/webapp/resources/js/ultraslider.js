e8b7ee81797ddd2fa7266e1abd7108cecbe15c21
cheolmin-Kim <kcm7582@naver.com>
2017-06-30 16:13:55 +0900

    ssss

---
+++ SpringWebProject/src/main/webapp/resources/js/ultraslider.js
@@ -0,0 +1,10 @@
+$("#handle2").roundSlider({
+    sliderType: "min-range",
+    radius: 130,
+    showTooltip: false,
+    width: 16,
+    value: 88,
+    handleSize: 0,
+    handleShape: "square",
+    circleShape: "half-top"
+});
--- SpringWebProject/src/main/webapp/resources/js/ultrasonicsensorchart.js
+++ SpringWebProject/src/main/webapp/resources/js/ultrasonicsensorchart.js
@@ -1,68 +1,131 @@
-var ultrasonicsensorchart;
+var ultrasonicSensorChart;
 
 $(function() {
-	 //차트객체 만드는 코드
-	ultrasonicSensorChart= new Highcharts.chart('container', {
+	var gaugeOptions = {
 
 		    chart: {
-		        polar: true,
+		        type: 'solidgauge',
 		        events: {
 					load: requestUltrasonicSensorChartData //함수이름(차트객체가 생성되고 나서 해당 함수를 실행시키는 코드)
 				}
 		    },
 
-		    title: {
-		        text: 'UltraSonic Chart'
-		    },
+		    title: null,
 
 		    pane: {
-		        startAngle: 0,
-		        endAngle: 360
-		    },
-
-		    xAxis: {
-		        tickInterval: 45,
-		        min: 0,
-		        max: 360,
-		        labels: {
-		            formatter: function () {
-		                return this.value + '°';
-		            }
+		        center: ['50%', '85%'],
+		        size: '140%',
+		        startAngle: -90,
+		        endAngle: 90,
+		        background: {
+		            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+		            innerRadius: '60%',
+		            outerRadius: '100%',
+		            shape: 'arc'
 		        }
 		    },
 
+		    tooltip: {
+		        enabled: false
+		    },
+
+		    // the value axis
 		    yAxis: {
-		        min: 0
+		        stops: [
+		            [0.1, '#55BF3B'], // green
+		            [0.5, '#DDDF0D'], // yellow
+		            [0.9, '#DF5353'] // red
+		        ],
+		        lineWidth: 0,
+		        minorTickInterval: null,
+		        tickAmount: 2,
+		        title: {
+		            y: -90    //거리 위치 올리기
+		        },
+		        labels: {
+		            y: 16
+		        }
 		    },
 
 		    plotOptions: {
-		        series: {
-		            pointStart: 0,
-		            pointInterval: 45
-		        },
-		        column: {
-		            pointPadding: 0,
-		            groupPadding: 0
+		        solidgauge: {
+		            dataLabels: {
+		                y: 5,
+		                borderWidth: 0,
+		                useHTML: true
+		            }
 		        }
-		    },
+		    }
+		};
+	
+	
+	
+	ultrasonicSensorChart=new Highcharts.chart('ultraSensorChartContainer', Highcharts.merge(gaugeOptions, {
+	    yAxis: {
+	        min: 0,
+	        max: 200,
+	        title: {
+	            text: '장애물과의 거리'
+	        }
+	    },
 
-		    series: [{
-		        type: 'column',
-		        name: 'Column',
-		        data: [],
-		        pointPlacement: 'between'
-		    }, {
-		        type: 'line',
-		        name: 'Line',
-		        data: [1, 2, 3, 4, 5, 6, 7, 8]
-		    }, {
-		        type: 'area',
-		        name: 'Area',
-		        data: [1, 8, 2, 7, 3, 6, 4, 5]
-		    }]
-		});
+	    credits: {
+	        enabled: false
+	    },
 
+	    series: [{
+	        name: 'Speed',
+	        data: [0],
+	        dataLabels: {
+	            format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+	                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'white') + '">{y}</span><br/>' +
+	                   '<span style="font-size:12px;color:silver">cm</span></div>'
+	        },
+	        tooltip: {
+	            valueSuffix: ' km/h'
+	        }
+	    }]
+
+	}));
+	
+	/*
+	setInterval(function () {
+	    // Speed
+	    var point,
+	        newVal,
+	        inc;
+
+	    if (chartSpeed) {
+	        point = chartSpeed.series[0].points[0];
+	        inc = Math.round((Math.random() - 0.5) * 100);
+	        newVal = point.y + inc;
+
+	        if (newVal < 0 || newVal > 200) {
+	            newVal = point.y - inc;
+	        }
+
+	        point.update(newVal);
+	    }
+
+	    // RPM
+	    if (chartRpm) {
+	        point = chartRpm.series[0].points[0];
+	        inc = Math.random() - 0.5;
+	        newVal = point.y + inc;
+
+	        if (newVal < 0 || newVal > 5) {
+	            newVal = point.y - inc;
+	        }
+
+	        point.update(newVal);
+	    }
+	}, 2000);
+	
+	*/
 });
+
+
+
 
 
 
@@ -71,8 +134,24 @@ function requestUltrasonicSensorChartData(){
 	var ws = new WebSocket("ws://"+location.host+"/SpringWebProject/websocket/ultrasonicsensor");
 	ws.onmessage = function(event){
 		var data= JSON.parse(event.data); //JSON.parse는 문자열로 되어있는 제이슨( '{"xxx":"value"}')을 javascript객체( {"xxx":"value"} )로 만들어주는 작업을함
-		var series =ultrasonicSensorChart.series[0];
-		var shift= series.data.length > 20; //화면이 이동되는 순간은 20개 이상일때 true를 리턴, 이하일때는 false
-		series.addPoint([data.time,data.distance],true,shift); //[x축,y축]
+		var point,value;
+		value=data.distance;
+	    if (ultrasonicSensorChart) {
+	        point = ultrasonicSensorChart.series[0].points[0];
+	      
+
+	        if (value > 200) {
+	            value=200;
+	        }
+	     
+
+	        point.update(value);
+	    }
+		//		var series =ultrasonicSensorChart.series[0];
+//		point.update(data.distance);
+//		var shift= series.data.length > 20; //화면이 이동되는 순간은 20개 이상일때 true를 리턴, 이하일때는 false
+//		series.addPoint([data.time,data.distance],true,shift); //[x축,y축]
 	};
 }
+
+
