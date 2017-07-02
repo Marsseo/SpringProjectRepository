67f9e709bd68fe0767102c8c220fb317afa95277
Marsseo <ghktjddl3@naver.com>
2017-07-03 06:59:05 +0900

    또 수정

---
+++ SensingCarFXproject/src/team1/HomeController.java
@@ -0,0 +1,33 @@
+package team1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+public class HomeController implements Initializable {
+
+	@FXML
+	private Label temperagture;
+	@FXML
+	private Label photoresistor;
+	@FXML
+	private Label gas;
+	@FXML
+	private Label distance;
+	@FXML
+	private Label angle;
+	@FXML
+	private Label speed;
+
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		
+		
+	}	
+	
+}
